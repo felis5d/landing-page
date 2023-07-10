85cc36cb90bb34df1c0f48ab0e6f8a47999cd889
felis5d <128646121+felis5d@users.noreply.github.com>
2023-07-10 03:35:39 +0200

    tweak styles and add animations

---
+++ script.js
@@ -0,0 +1,8 @@
+function subscribe() {
+    let button = document.querySelector("#subscribe-button");
+    button.innerText = "Subscribed!";
+    let signUp = document.querySelector("#sign-up");
+    signUp.innerText = "Thank you for signing up!";
+    let missOut = document.querySelector("#miss-out");
+    missOut.innerText = "Now you won't miss out on any exiting news \n from the F-0x star cluster.";
+}
